da42dacc10701d4584a1df88bd8305838e3022ea
shahrukh <sharukhsyed.hussainy@gmail.com>
2025-04-03 17:06:39 +0530

    fix: loading logo alignment fixed

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,14 @@ import LoadingLogo from "@/components/UI/LoadingLogo"
 
 const Loading = () => {
   return (
-    <Screen safeAreaEdges={["top"]}>
+    <Screen
+      safeAreaEdges={["top"]}
+      contentContainerStyle={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <LoadingLogo />
     </Screen>
   )
