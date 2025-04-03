42ba8839297432069ffc5855e1301d2e59bfe972
shahrukh <sharukhsyed.hussainy@gmail.com>
2025-04-03 16:52:04 +0530

    fix: modal spinner aligned to centered

--- src/components/UI/ModalSpinner.tsx
+++ src/components/UI/ModalSpinner.tsx
@@ -10,13 +10,8 @@ type Props = {
 const ModalSpinner = ({ isLoading }: Props) => {
   const { themed } = useAppTheme()
   return (
-    <Modal visible={isLoading}>
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
+    <Modal visible={isLoading} transparent animationType="none" statusBarTranslucent>
+      <View style={themed($modalWrapper)}>
         <View style={themed($modalBackground)}>
           <Spinner size={28} />
         </View>
@@ -31,4 +26,12 @@ const $modalBackground: ThemedStyle<ViewStyle> = ({ colors }) => ({
   backgroundColor: colors.background,
   padding: 14,
   borderRadius: 20,
+})
+
+const $modalWrapper: ThemedStyle<ViewStyle> = ({ colors, spacing }) => ({
+  flex: 1,
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: colors.palette.overlay50,
+  padding: spacing.md,
 })
