c5a6ecb6d73557e20d704954d4d3f6368f3a1eb1
shahrukh <sharukhsyed.hussainy@gmail.com>
2025-04-04 13:26:13 +0530

    feat: some changes

--- src/app/(auth)/(tabs)/challenges.tsx
+++ src/app/(auth)/(tabs)/challenges.tsx
@@ -1,11 +1,36 @@
 import { View, Text } from "react-native"
 import React from "react"
+import { Screen } from "@/components"
+import PulseDot from "@/components/UI/PulseDot"
 
 const ChallengesScreen = () => {
   return (
-    <View>
-      <Text>ChallengesScreen</Text>
-    </View>
+    <Screen
+      safeAreaEdges={["top"]}
+      contentContainerStyle={{
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 6,
+          }}
+        >
+          <PulseDot delay={100} />
+          <PulseDot delay={150} />
+          <PulseDot delay={200} />
+        </View>
+      </View>
+    </Screen>
   )
 }
 
