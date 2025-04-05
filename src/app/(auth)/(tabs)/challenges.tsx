dd4af17d8d556c839aa78a3b3e1e29961372e615
shahrukh <sharukhsyed.hussainy@gmail.com>
2025-04-06 01:14:48 +0530

    feat: add challenges list

--- src/app/(auth)/(tabs)/challenges.tsx
+++ src/app/(auth)/(tabs)/challenges.tsx
@@ -2,6 +2,9 @@ import { View, Text } from "react-native"
 import React from "react"
 import { Screen } from "@/components"
 import PulseDot from "@/components/UI/PulseDot"
+import TopBar from "@/components/UI/TopBar"
+import FAB from "@/components/UI/Fab"
+import MyChallengesList from "@/components/Challenges/MyChallengesList"
 
 const ChallengesScreen = () => {
   return (
@@ -10,7 +13,11 @@ const ChallengesScreen = () => {
       contentContainerStyle={{
         flex: 1,
       }}
-    ></Screen>
+    >
+      <TopBar showBackButton={false} title="Challenges" />
+      <MyChallengesList />
+      <FAB />
+    </Screen>
   )
 }
 
