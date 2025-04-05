c5772c9f2badc5b1316c2336e9fb6983c820dff7
shahrukh <sharukhsyed.hussainy@gmail.com>
2025-04-05 10:43:32 +0530

    feat: add actionbutton to go back

--- src/components/UI/TopBar.tsx
+++ src/components/UI/TopBar.tsx
@@ -8,20 +8,14 @@ import ActionButton from "./ActionButton"
 type Props = {
   title?: string
   showBackButton?: boolean
+  onBackButtonPressed: () => void
 }
-const TopBar = ({ title, showBackButton = true }: Props) => {
+const TopBar = ({ title, showBackButton = true, onBackButtonPressed }: Props) => {
   const { themed } = useAppTheme()
   return (
     <View style={themed($topHeader)}>
       <View style={$headerAction}>
-        {showBackButton && (
-          <ActionButton
-            icon="chevron-left"
-            onPress={() => {
-              router.back()
-            }}
-          />
-        )}
+        {showBackButton && <ActionButton icon="chevron-left" onPress={onBackButtonPressed} />}
         {title && (
           <Text size="lg" weight="bold" style={themed($title)}>
             {title}
