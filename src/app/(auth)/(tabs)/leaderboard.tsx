689c44058c32f4204884f4eb7e85ba75adc91d48
shahrukh <sharukhsyed.hussainy@gmail.com>
2025-04-06 10:19:06 +0530

    feat: add leaderboard screen

--- src/app/(auth)/(tabs)/leaderboard.tsx
+++ src/app/(auth)/(tabs)/leaderboard.tsx
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native"
-import React from "react"
+import { TouchableOpacity, View } from "react-native"
+import React, { useCallback, useMemo, useState } from "react"
+import { Screen, Text } from "@/components"
+import TopBar from "@/components/UI/TopBar"
+import { useUser } from "@clerk/clerk-expo"
+import { useAppTheme } from "@/utils/useAppTheme"
+import LeaderboardTopHeader from "@/components/Leaderboard/LeaderboardTopHeader"
+import { usePaginatedQuery } from "convex/react"
+import { api } from "convex/_generated/api"
+import WeeklyCountDownTimer from "@/components/Leaderboard/WeeklyCountDownTimer"
+import UsersLeaderboardList from "@/components/Leaderboard/UsersLeaderboardList"
 
 const Leaderboard = () => {
+  const { user } = useUser()
+  const [tabName, setTabName] = useState("weekly")
+
   return (
-    <View>
-      <Text>Leaderboard</Text>
-    </View>
+    <Screen
+      safeAreaEdges={["top"]}
+      contentContainerStyle={{
+        flex: 1,
+      }}
+    >
+      <TopBar showBackButton={false} title="Leaderboard" />
+      <LeaderboardTopHeader tabName={tabName} setTabName={setTabName} />
+      {tabName === "weekly" ? <WeeklyCountDownTimer /> : null}
+      <UsersLeaderboardList tabName={tabName} />
+    </Screen>
   )
 }
 
