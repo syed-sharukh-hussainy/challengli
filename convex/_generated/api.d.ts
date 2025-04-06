e06fb66112518dd48e219ad20095b006a93b23dc
shahrukh <sharukhsyed.hussainy@gmail.com>
2025-04-06 23:42:56 +0530

    some updates

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,9 +13,11 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as achievements from "../achievements.js";
 import type * as categories from "../categories.js";
 import type * as clerk from "../clerk.js";
 import type * as http from "../http.js";
+import type * as leaderboard from "../leaderboard.js";
 import type * as presetChallenges from "../presetChallenges.js";
 import type * as userChallenges from "../userChallenges.js";
 import type * as users from "../users.js";
@@ -30,9 +32,11 @@ import type * as utils from "../utils.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  achievements: typeof achievements;
   categories: typeof categories;
   clerk: typeof clerk;
   http: typeof http;
+  leaderboard: typeof leaderboard;
   presetChallenges: typeof presetChallenges;
   userChallenges: typeof userChallenges;
   users: typeof users;
